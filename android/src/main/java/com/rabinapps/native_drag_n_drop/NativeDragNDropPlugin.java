1e499cf72c8d50a175b28fe8491c4223d09b31b8
getBoolean <danielwnewville@gmail.com>
2022-01-26 14:26:14 -0600

    Add ActivityAware

--- android/src/main/java/com/rabinapps/native_drag_n_drop/NativeDragNDropPlugin.java
+++ android/src/main/java/com/rabinapps/native_drag_n_drop/NativeDragNDropPlugin.java
@@ -3,13 +3,15 @@ package com.rabinapps.native_drag_n_drop;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** NativeDragNDropPlugin */
-public class NativeDragNDropPlugin implements FlutterPlugin, MethodCallHandler {
+public class NativeDragNDropPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -34,5 +36,29 @@ public class NativeDragNDropPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+    // TODO: your plugin is now attached to an Activity
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    // TODO: the Activity your plugin was attached to was
+    // destroyed to change configuration.
+    // This call will be followed by onReattachedToActivityForConfigChanges().
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+    // TODO: your plugin is now attached to a new Activity
+    // after a configuration change.
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    // TODO: your plugin is no longer associated with an Activity.
+    // Clean up references.
   }
 }
