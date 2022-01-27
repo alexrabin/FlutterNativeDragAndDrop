fe620fe549638921855326af12c11babfa89549f
getBoolean <danielwnewville@gmail.com>
2022-01-27 00:36:14 -0600

    start listening again when attached to activity

--- android/src/main/java/com/rabinapps/native_drag_n_drop/NativeDropView.java
+++ android/src/main/java/com/rabinapps/native_drag_n_drop/NativeDropView.java
@@ -196,7 +196,6 @@ public class NativeDropView implements PlatformView, MethodChannel.MethodCallHan
                 final Map<String, Object> urlMap = new HashMap<String, Object>();
                 urlMap.put("image", uri.getPath());
                 data.add(urlMap);
-//                sendDropData(String.format("[{\"image\": \"%s\"}]", uri.getPath()));
             } else {
                 // Other schemes (such as "android.resource") do not require a permission grant.
                 final ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
@@ -204,7 +203,6 @@ public class NativeDropView implements PlatformView, MethodChannel.MethodCallHan
                 urlMap.put("image", uri.getPath());
                 data.add(urlMap);
                 sendDropData(data);
-//                sendDropData(String.format("[{\"image\": \"%s\"}]", uri.getPath()));
             }
         }
     }
@@ -242,6 +240,7 @@ public class NativeDropView implements PlatformView, MethodChannel.MethodCallHan
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         activity = binding.getActivity();
+        dragView.setOnDragListener(viewDragListener());
     }
 
     @Override
